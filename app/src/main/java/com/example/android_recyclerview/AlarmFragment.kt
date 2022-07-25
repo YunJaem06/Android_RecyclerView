48c38898aa03e84f37eeb29a61381e6e42e90675
YunJaem06 <yunjae0616@gmail.com>
2022-07-25 22:29:08 +0900

    Recyclerview bottom navigation 적용

---
+++ app/src/main/java/com/example/android_recyclerview/AlarmFragment.kt
@@ -0,0 +1,22 @@
+package com.example.android_recyclerview
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.android_recyclerview.databinding.FragmentAlarmBinding
+
+class AlarmFragment: Fragment() {
+
+    private lateinit var binding : FragmentAlarmBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentAlarmBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
--- app/src/main/java/com/example/android_recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/android_recyclerview/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.android_recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.android_recyclerview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction().add(R.id.fl_main_layout, AlarmFragment()).commit()
+
+        binding.navigationMain.setOnItemSelectedListener {
+            replaceFragment(
+                when(it.itemId) {
+                    R.id.navigation_alarm -> AlarmFragment()
+                    R.id.navigation_sleep -> SleepFragment()
+                    R.id.navigation_favorite -> FavoriteFragment()
+                    R.id.navigation_news -> NewsFragment()
+                    else -> MoreFragment()
+                }
+            )
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().replace(R.id.fl_main_layout, fragment).commit()
     }
 }
