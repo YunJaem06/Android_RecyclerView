f4ec748a6aa0864b7f506d3a06ad0eceab29b1db
YunJaem06 <yunjae0616@gmail.com>
2022-07-26 18:24:27 +0900

    Recyclerview alarm layout 구현 & Recyclerview 구현준비

--- app/src/main/java/com/example/android_recyclerview/AlarmFragment.kt
+++ app/src/main/java/com/example/android_recyclerview/AlarmFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.android_recyclerview.databinding.FragmentAlarmBinding
 
 class AlarmFragment: Fragment() {
@@ -18,5 +19,6 @@ class AlarmFragment: Fragment() {
     ): View? {
         binding = FragmentAlarmBinding.inflate(inflater, container, false)
         return binding.root
+
     }
 }
